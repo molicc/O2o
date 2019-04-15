cbc1490a54daf207fa19322af71c82fe37b13041
fengtao256 <fengtao256@qq.com>
2019-04-15 23:25:26 +0800

    优化：数据提交成功则刷新数据区域

--- src/main/webapp/resources/js/superadmin/arealist.js
+++ src/main/webapp/resources/js/superadmin/arealist.js
@@ -4,28 +4,31 @@ $(function () {
     var modifAreaUrl = "/superadmin/modifyareabyid";
     var addAreaUrl = "/superadmin/addarea";
     //1、获取区域列表，并拼接到html $.getJSON(url,function(){})
-    $.getJSON(listAreaUrl,function (data) {
-        //请求成功，则html平拼接
-        if(data.success){
-            var count = data.count ;
-            var html = '' ;
-            data.arealist.map(function (value) {
-                html += ''+
-                    '<div class="row no-gutter" style="font-size: 14px">'+
-                    '<div class="col-20"'+"area-id="+value.areaId+'>'+value.areaName+'</div>'+
-                    '<div class="col-10">'+value.priority+'</div>'+
-                    '<div class="col-40" >'+"&emsp;&emsp13;"+new Date(value.lastEditTime).Format("yyyy-MM-dd")+'</div>'+
-                    '<div class="col-15"><a class="modify button button-success"'+"area-id="+value.areaId+'>'+"修改"+'</a></div>'+
-                    '<div class="col-15"><a class="submit button button-submit"'+"area-id="+value.areaId+'>'+"提交"+'</a></div>'+
-                    '</div>'+
-                    '<hr/>';
-            })
-            //添加到area-wrap
-            $('.area-wrap').html(html);
-        }else {
-            $.toast("拉取初始化信息失败");
-        }
-    })
+    function getAreaList(){
+        $.getJSON(listAreaUrl,function (data) {
+            //请求成功，则html平拼接
+            if(data.success){
+                var count = data.count ;
+                var html = '' ;
+                data.arealist.map(function (value) {
+                    html += ''+
+                        '<div class="row no-gutter" style="font-size: 14px">'+
+                        '<div class="col-20"'+"area-id="+value.areaId+'>'+value.areaName+'</div>'+
+                        '<div class="col-10">'+value.priority+'</div>'+
+                        '<div class="col-40" >'+"&emsp;&emsp13;"+new Date(value.lastEditTime).Format("yyyy-MM-dd")+'</div>'+
+                        '<div class="col-15"><a class="modify button button-success"'+"area-id="+value.areaId+'>'+"修改"+'</a></div>'+
+                        '<div class="col-15"><a class="submit button button-submit"'+"area-id="+value.areaId+'>'+"提交"+'</a></div>'+
+                        '</div>'+
+                        '<hr/>';
+                })
+                //添加到area-wrap
+                $('.area-wrap').html(html);
+            }else {
+                $.toast("拉取初始化信息失败");
+            }
+        })
+    }
+    getAreaList();
     //2、监听修改按钮，按下确认就修改这个html，让这个html变为添加框，修改类容，做ajax提交
     $(document).on("click",".modify",function (e) {
         $.confirm($(this).attr("area-id"));
@@ -58,7 +61,8 @@ $(function () {
                         success:function(data){
                             if(data.success){
                                 $.toast("提交成功");
-
+                                //刷新区域列表
+                                getAreaList();
                             }else{
                                 $.toast(data.errMsg);
                             }
@@ -72,6 +76,7 @@ $(function () {
     $(document).on("click",".delete",function (e) {
         $('.tempHtml').remove();
     });
+    //todo：对修改按钮和提交刷新做改变
     //初始化界面
     //$.init();
 });
